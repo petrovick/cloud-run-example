a5c3ba9706ee6cfc7aab69e3d271ba4828c2ea24
Macbook <macbook@Macbooks-MacBook-Pro.local>
2020-03-28 15:35:52 -0300

    controller has being changed

--- src/controllers/v1/test.controller.js
+++ src/controllers/v1/test.controller.js
@@ -1,5 +1,5 @@
 const index = async (req, res) => {
-  return res.status(200).json({ message: `Woking as expected 3.` })
+  return res.status(200).json({ message: `Woking as expected 4.` })
 }
 
 module.exports = {
